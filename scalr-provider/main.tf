d20c55b60b0d38c618d713eb7aa921cafdfc45e6
Olga Stasyuk <ostasyuk@users.noreply.github.com>
2022-07-27 18:11:56 +0300

    Update main.tf

--- scalr-provider/main.tf
+++ scalr-provider/main.tf
@@ -25,29 +25,29 @@ variable "scalr_hostname" {
 }
 
 
-resource "scalr_provider_configuration" "scalr" {
-  name                   = "scalr"
-  account_id             = var.account_id
-  export_shell_variables = false
-  environments           = ["*"]
-  scalr {
-    token    = var.scalr_token
-    hostname = var.scalr_hostname
-  }
-}
+# resource "scalr_provider_configuration" "scalr" {
+#   name                   = "scalr"
+#   account_id             = var.account_id
+#   export_shell_variables = false
+#   environments           = ["*"]
+#   scalr {
+#     token    = var.scalr_token
+#     hostname = var.scalr_hostname
+#   }
+# }
 # resource "scalr_environment" "scalrpcfgtest" {
 #   name                    = "pcfg-test-${var.prefix}"
 #   account_id              = var.account_id
 #   cost_estimation_enabled = false
 # }
 
-# resource "scalr_vcs_provider" "vcs" {
-#   name = "git-aprrr"
-#   account_id = var.account_id
-#   # environment_id = local.environment_id
-#   vcs_type = "github"
-#   token = "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"
-# }
+resource "scalr_vcs_provider" "vcs" {
+  name = "git-aprrr"
+  account_id = var.account_id
+  # environment_id = local.environment_id
+  vcs_type = "github"
+  token = "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"
+}
 
 # resource "scalr_workspace" "scalrpcfgtest" {
 #   name              = "workspace-pcfg-test-${var.prefix}"
