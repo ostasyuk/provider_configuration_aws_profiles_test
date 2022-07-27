21b898e5d1ea61c0111781baa13219228075c3d5
Olga Stasyuk <ostasyuk@users.noreply.github.com>
2022-07-27 18:26:59 +0300

    Update main.tf

--- prov-test/main.tf
+++ prov-test/main.tf
@@ -1,11 +1,17 @@
 provider "scalr" { }
 
-resource "null_resource" "env_varstimeo" {
-  count = 3
-  provisioner "local-exec" {
-    command = "echo $ENV"
-    environment = {
-      ENV = "Hello World"
-    }
- }
+resource "scalr_workspace" "vcs-driven" {
+  name            = "my-workspace-name-new"
+  environment_id  = "env-svrcnchebt61e30"
+  execution_mode = "local"
 }
+
+# resource "null_resource" "env_varstimeo" {
+#   count = 3
+#   provisioner "local-exec" {
+#     command = "echo $ENV"
+#     environment = {
+#       ENV = "Hello World"
+#     }
+#  }
+# }
