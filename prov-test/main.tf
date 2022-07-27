551b80b48e9a29fdd9f4a5b4a1592d844bc5c1f2
Olga Stasyuk <ostasyuk@users.noreply.github.com>
2022-07-27 18:15:59 +0300

    Create main.tf

---
+++ prov-test/main.tf
@@ -0,0 +1,11 @@
+provider scalr {}
+
+resource "null_resource" "env_varstimeo" {
+  count = 3
+  provisioner "local-exec" {
+    command = "echo $ENV"
+    environment = {
+      ENV = "Hello World"
+    }
+ }
+}
