6c69c500859e06248cec2342dc412428f7f96417
Olga Stasyuk <ostasyuk@users.noreply.github.com>
2022-07-27 18:33:14 +0300

    Update main.tf

--- prov-test/main.tf
+++ prov-test/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    scalr = {
+      source  = "registry.scalr.dev/scalr/scalr"
+      version = "1.0.0-rc-develop"
+    }
+  }
+}
 provider "scalr" { }
 
 resource "scalr_workspace" "vcs-driven" {
