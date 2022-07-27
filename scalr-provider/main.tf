034b757aa662d25c111e7f61a06ce6787ca9f6d8
Olga Stasyuk <ostasyuk@users.noreply.github.com>
2022-07-27 18:00:29 +0300

    Update main.tf

--- scalr-provider/main.tf
+++ scalr-provider/main.tf
@@ -26,7 +26,7 @@ variable "scalr_hostname" {
 
 
 resource "scalr_provider_configuration" "scalr" {
-  name                   = "scalrpcfg"
+  name                   = "scalr"
   account_id             = var.account_id
   export_shell_variables = false
   environments           = ["*"]
@@ -35,58 +35,58 @@ resource "scalr_provider_configuration" "scalr" {
     hostname = var.scalr_hostname
   }
 }
-resource "scalr_environment" "scalrpcfgtest" {
-  name                    = "pcfg-test-${var.prefix}"
-  account_id              = var.account_id
-  cost_estimation_enabled = false
-}
+# resource "scalr_environment" "scalrpcfgtest" {
+#   name                    = "pcfg-test-${var.prefix}"
+#   account_id              = var.account_id
+#   cost_estimation_enabled = false
+# }
 
-resource "scalr_vcs_provider" "vcs" {
-  name = "git-aprrr"
-  account_id = var.account_id
-  # environment_id = local.environment_id
-  vcs_type = "github"
-  token = "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"
-}
+# resource "scalr_vcs_provider" "vcs" {
+#   name = "git-aprrr"
+#   account_id = var.account_id
+#   # environment_id = local.environment_id
+#   vcs_type = "github"
+#   token = "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"
+# }
 
-resource "scalr_workspace" "scalrpcfgtest" {
-  name              = "workspace-pcfg-test-${var.prefix}"
-  environment_id    = scalr_environment.scalrpcfgtest.id
-  auto_apply        = false
-  operations        = false
-  vcs_provider_id   = scalr_vcs_provider.vcs.id
-  working_directory = "scalr_pcfg_create_workspace"
-  vcs_repo {
-    identifier = "ostasyuk/test-github-app"
-    path  = "null"
-    branch     = "master"
-  }
+# resource "scalr_workspace" "scalrpcfgtest" {
+#   name              = "workspace-pcfg-test-${var.prefix}"
+#   environment_id    = scalr_environment.scalrpcfgtest.id
+#   auto_apply        = false
+#   operations        = false
+#   vcs_provider_id   = scalr_vcs_provider.vcs.id
+#   working_directory = "scalr_pcfg_create_workspace"
+#   vcs_repo {
+#     identifier = "ostasyuk/test-github-app"
+#     path  = "null"
+#     branch     = "master"
+#   }
 
-  provider_configuration {
-    id = scalr_provider_configuration.scalr.id
-  }
-}
+#   provider_configuration {
+#     id = scalr_provider_configuration.scalr.id
+#   }
+# }
 
-resource "scalr_variable" "prefix" {
-  key            = "prefix"
-  value          = "${var.prefix}_slave"
-  category       = "terraform"
-  environment_id = scalr_environment.scalrpcfgtest.id
-  workspace_id   = scalr_workspace.scalrpcfgtest.id
-}
+# resource "scalr_variable" "prefix" {
+#   key            = "prefix"
+#   value          = "${var.prefix}_slave"
+#   category       = "terraform"
+#   environment_id = scalr_environment.scalrpcfgtest.id
+#   workspace_id   = scalr_workspace.scalrpcfgtest.id
+# }
 
-resource "scalr_variable" "scalr_token" {
-  key            = "scalr_token"
-  value          = var.scalr_token
-  category       = "terraform"
-  environment_id = scalr_environment.scalrpcfgtest.id
-  workspace_id   = scalr_workspace.scalrpcfgtest.id
-}
+# resource "scalr_variable" "scalr_token" {
+#   key            = "scalr_token"
+#   value          = var.scalr_token
+#   category       = "terraform"
+#   environment_id = scalr_environment.scalrpcfgtest.id
+#   workspace_id   = scalr_workspace.scalrpcfgtest.id
+# }
 
-resource "scalr_variable" "scalr_hostname" {
-  key            = "scalr_hostname"
-  value          = var.scalr_hostname
-  category       = "terraform"
-  environment_id = scalr_environment.scalrpcfgtest.id
-  workspace_id   = scalr_workspace.scalrpcfgtest.id
-}
+# resource "scalr_variable" "scalr_hostname" {
+#   key            = "scalr_hostname"
+#   value          = var.scalr_hostname
+#   category       = "terraform"
+#   environment_id = scalr_environment.scalrpcfgtest.id
+#   workspace_id   = scalr_workspace.scalrpcfgtest.id
+# }
